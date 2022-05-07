f11326d7e20b1bf68d5de43d4332631a2163c06d
Carol Sousa <carolineramos98@outlook.com.br>
2022-05-07 15:24:16 -0300

    Aulas e Atividades

---
+++ Atividade1/src/main/java/com/mycompany/atividade1/Caneta.java
@@ -0,0 +1,34 @@
+package com.mycompany.atividade1;
+
+public class Caneta {
+
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    Boolean tampado;
+
+    public void print() {
+        System.out.println("Modelo : " + modelo);
+        System.out.println("Cor : " + cor);
+        System.out.println("Ponta : " + ponta);
+        System.out.println("Carga : " + carga + "%");
+        System.out.println("Tampado : " + tampado);
+    }
+
+    public void estado() {
+        if (tampado == true) {
+            System.out.println("Está tampado e não pode Rabiscar!\n");
+        } else {
+            System.out.println("Rabiscando...\n");
+        }
+    }
+
+    public Boolean tampar() {
+        return tampado = true;
+    }
+
+    public Boolean destampar() {
+        return tampado = false;
+    }
+}
